f80d5fb36eb36b7f0a6f734a68d9bca3536f5053
Ludwig Stecher <ludwig.stecher@gmx.de>
2023-03-18 23:44:11 +0100

    update help

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,32 @@ mod lexer;
 pub type StdError = Box<dyn error::Error>;
 
 #[derive(Parser)]
-#[command(author, version, about, max_term_width = 100)]
+#[command(
+    author,
+    version,
+    about,
+    max_term_width = 100,
+    help_template = "\
+{before-help}{name} {version}
+{author-with-newline}{about-with-newline}
+{usage-heading} {usage}
+
+{all-args}{after-help}"
+)]
 struct Cli {
-    /// The command(s) to execute
+    /// The command(s) to execute. Must be wrapped in quotes.
     #[arg(required = true)]
-    command: Vec<String>,
+    commands: Vec<String>,
     /// The shell to run the command in. On macOS and FreeBSD, the shell has to support
     /// `-c <command>`
     #[arg(short, long)]
     shell: Option<String>,
     /// Programs that have subcommands (which should be highlighted). Multiple arguments are
-    /// separated with a comma, e.g. to-html -l git,cargo,npm 'git checkout main'
+    /// separated with a comma, e.g. `to-html -l git,cargo,npm "git checkout main"`
     #[arg(short = 'l', long, value_delimiter = ',')]
     highlight: Vec<String>,
-    /// Prefix for CSS classes. For example, with the 'to-html' prefix, the 'arg' class becomes
-    /// 'to-html-arg'",
+    /// Prefix for CSS classes. For example, with the `to-html` prefix, the `arg` class becomes
+    /// `to-html-arg`
     #[arg(short, long)]
     prefix: Option<String>,
     /// Don't run the commands, just emit the HTML for the command prompt
@@ -31,7 +42,7 @@ struct Cli {
     /// Print the (abbreviated) current working directory in the command prompt
     #[arg(short, long)]
     cwd: bool,
-    /// Output a complete HTML document, not just a <pre>
+    /// Output a complete HTML document, not just a `<pre>`
     #[arg(short, long)]
     doc: bool,
 }
@@ -56,7 +67,7 @@ impl From<Cli> for Args {
             no_run,
             cwd,
             doc,
-            command: commands,
+            commands,
         } = cli;
 
         let prefix = prefix.map(|s| format!("{}-", Esc(s))).unwrap_or_default();
