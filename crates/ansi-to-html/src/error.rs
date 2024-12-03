c4fb9a646f8d8cc18fbc3da5aba57298cb791b08
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-12-03 00:14:27 -0700

    refactor(ansi-to-html): Drop `thiserror` for a manual impl

--- crates/ansi-to-html/src/error.rs
+++ crates/ansi-to-html/src/error.rs
@@ -1,16 +1,31 @@
-use std::num::ParseIntError;
+use std::{fmt, num::ParseIntError};
 
 /// Errors that can occur when converting an ANSI string to HTML
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug)]
 pub enum Error {
     /// Parsing a number was unsuccessful
-    #[error(transparent)]
-    ParseInt(#[from] ParseIntError),
+    ParseInt(ParseIntError),
 
     /// The ANSI escape code is invalid
-    #[error("Invalid ANSI: {msg}")]
     InvalidAnsi { msg: String },
 }
+
+impl From<ParseIntError> for Error {
+    fn from(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::ParseInt(err) => write!(f, "{}", err),
+            Self::InvalidAnsi { msg } => write!(f, "Invalid ANSI: {}", msg),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
 
 impl Error {
     pub(crate) fn invalid_ansi(s: &'static str) -> impl Fn() -> Self {
