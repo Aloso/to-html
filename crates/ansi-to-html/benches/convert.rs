bdb0ec61f4a268d0f4a63ff33b04632298ccd233
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-01-26 21:37:47 -0700

    bench(ansi-to-html): Benchmark various `Converter` options

--- crates/ansi-to-html/benches/convert.rs
+++ crates/ansi-to-html/benches/convert.rs
@@ -1,5 +1,6 @@
 use std::{hint::black_box, io::Read, time::Duration};
 
+use ansi_to_html::Converter;
 use divan::{bench, counter::BytesCount, Bencher, Divan};
 use flate2::bufread::GzDecoder;
 
@@ -12,14 +13,52 @@ fn main() {
 
 static COMPRESSED_TERMINAL_SESSION: &[u8] = include_bytes!("../assets/terminal_session.gz");
 
-#[bench]
-fn convert(bencher: Bencher) {
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Input {
+    AnsiHeavy,
+    PlainText,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum MaybeEsc {
+    Esc,
+    SkipEsc,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum MaybeOpt {
+    Opt,
+    SkipOpt,
+}
+
+#[bench(args = [
+    (Input::AnsiHeavy, MaybeEsc::Esc, MaybeOpt::Opt),
+    (Input::AnsiHeavy, MaybeEsc::Esc, MaybeOpt::SkipOpt),
+    (Input::AnsiHeavy, MaybeEsc::SkipEsc, MaybeOpt::Opt),
+    (Input::AnsiHeavy, MaybeEsc::SkipEsc, MaybeOpt::SkipOpt),
+    (Input::PlainText, MaybeEsc::Esc, MaybeOpt::Opt),
+    (Input::PlainText, MaybeEsc::Esc, MaybeOpt::SkipOpt),
+    (Input::PlainText, MaybeEsc::SkipEsc, MaybeOpt::Opt),
+    (Input::PlainText, MaybeEsc::SkipEsc, MaybeOpt::SkipOpt),
+])]
+fn convert(bencher: Bencher, (input, esc, opt): (Input, MaybeEsc, MaybeOpt)) {
     let mut decoder = GzDecoder::new(COMPRESSED_TERMINAL_SESSION);
     let mut terminal_session = String::new();
     decoder.read_to_string(&mut terminal_session).unwrap();
 
+    if input == Input::PlainText {
+        // Replace the start of all ansi escape sequences with a benign character to "strip" all of
+        // the ansi codes (well replace them with non-ansi garbage)
+        terminal_session = terminal_session.replace('\u{1b}', "~");
+    }
+
     let bytes_counter = BytesCount::of_str(&terminal_session);
+
+    let converter = Converter::new()
+        .skip_escape(esc == MaybeEsc::SkipEsc)
+        .skip_optimize(opt == MaybeOpt::SkipOpt);
+
     bencher
         .counter(bytes_counter)
-        .bench(|| ansi_to_html::convert(black_box(&terminal_session)).unwrap());
+        .bench(|| converter.convert(black_box(&terminal_session)).unwrap());
 }
