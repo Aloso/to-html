3450d6a488599e233fb4f94173502d378d006728
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-09-30 00:34:43 -0400

    fuzz: rename `fuzz/utils` -> `fuzz/harness`

--- crates/ansi-to-html/fuzz/fuzz_targets/opt_equiv.rs
+++ crates/ansi-to-html/fuzz/fuzz_targets/opt_equiv.rs
@@ -3,5 +3,5 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|ansi_text: &str| {
-    html_interpreter::assert_opt_equiv_to_no_opt(ansi_text);
+    harness::assert_opt_equiv_to_no_opt(ansi_text);
 });
